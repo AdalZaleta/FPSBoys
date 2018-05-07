c4d2c07c1e89b11380bed7c95b665aa7319b8ceb
Retus001 <retus001@gmail.com>
2018-05-07 16:24:29 -0500

    EvilBoi
    Recieve Damage

---
+++ Assets/_FPSBoys/Characters/Enemies/EvilBoi/EvilBoi_Behaviour.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSBoys{
+	public class EvilBoi_Behaviour : MonoBehaviour {
+
+		public int HP;
+		int freezeCount;
+		int shockdmg = 1;
+		public GameObject model;
+
+		public void RecieveDmg(int type, int dmg)
+		{
+			switch (type)
+			{
+			case 1:
+				// Default Bullet Event
+				HP -= dmg;
+				StartCoroutine (blink ());
+				break;
+			case 2:
+				// Ice Bullet Event
+				HP -= dmg;
+				freezeCount++;
+				StartCoroutine (blink ());
+				break;
+			case 3:
+				// Lightning Bullet Event
+				HP -= dmg;
+				StartCoroutine (shock (3));
+				StartCoroutine (blink ());
+				break;
+			default:
+				Debug.Log ("Switch entered default");
+				break;
+			}
+		}
+
+		IEnumerator blink()
+		{
+			model.GetComponent<SkinnedMeshRenderer> ().material.color = Color.red;
+			yield return new WaitForSeconds (0.2f);
+			model.GetComponent<SkinnedMeshRenderer> ().material.color = Color.white;
+			yield return new WaitForSeconds (0.2f);
+			model.GetComponent<SkinnedMeshRenderer> ().material.color = Color.red;
+			yield return new WaitForSeconds (0.2f);
+			model.GetComponent<SkinnedMeshRenderer> ().material.color = Color.white;
+		}
+
+		IEnumerator shock(int time)
+		{
+			yield return new WaitForSeconds (1.0f);
+			HP -= shockdmg;
+			if (time > 0)
+			{
+				time--;
+				StartCoroutine (shock (time));
+			}
+		}
+	}
+}
---
+++ Assets/_FPSBoys/Temp_Testing/GodsHand.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSBoys{
+	public class GodsHand : MonoBehaviour {
+
+		public GameObject enemy;
+
+		// Use this for initialization
+		void Start () {
+			
+		}
+		
+		// Update is called once per frame
+		void Update () {
+			if (Input.GetKeyDown(KeyCode.Alpha1))
+			{
+				enemy.GetComponent<EvilBoi_Behaviour> ().RecieveDmg (1, 10);
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha2))
+			{
+				enemy.GetComponent<EvilBoi_Behaviour> ().RecieveDmg (2, 20);
+			}
+			if (Input.GetKeyDown(KeyCode.Alpha3))
+			{
+				enemy.GetComponent<EvilBoi_Behaviour> ().RecieveDmg (3, 30);
+			}
+		}
+	}
+}
