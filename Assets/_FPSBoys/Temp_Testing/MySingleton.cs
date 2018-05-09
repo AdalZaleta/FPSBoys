d48ddc19522e2b7b9b31b40d2e47a1b03c399a80
Retus001 <retus001@gmail.com>
2018-05-09 12:23:25 -0500

    Singleton/Delegates Test
    temp - can ignore

---
+++ Assets/_FPSBoys/Temp_Testing/MySingleton.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MySingleton : MonoBehaviour {
+
+	private static MySingleton m_instance;
+
+	public static MySingleton Instance
+	{
+		get
+		{
+			if (m_instance == null)
+			{
+				GameObject go = new GameObject ("My Singleton");
+				go.AddComponent<MySingleton> ();
+
+				DontDestroyOnLoad (go);
+			}
+			return m_instance;
+		}
+	}
+
+	void Awake()
+	{
+		if (m_instance)
+		{
+			Destroy (gameObject);
+		}
+		else
+		{
+			m_instance = this;
+		}
+	}
+
+	// Delegate Logic //
+
+	public void Initialize()
+	{
+		
+	}
+
+	public delegate void IntTemplate(int newInt);
+
+	public static IntTemplate AddScoreHandler;
+	public static IntTemplate DamagePlayerHandler;
+
+	public void Addscore(int newScore)
+	{
+		AddScoreHandler (newScore);
+	}
+
+	public void DamagePlayer(int damageValue)
+	{
+		DamagePlayerHandler (damageValue);
+	}
+}
---
+++ Assets/_FPSBoys/Temp_Testing/UseSingleton.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UseSingleton : MonoBehaviour {
+
+	void Update()
+	{
+		if (Input.GetKeyDown(KeyCode.Space))
+		{
+			MySingleton.Instance.Addscore (50);
+		}
+		if (Input.GetKeyDown(KeyCode.F))
+		{
+			MySingleton.Instance.DamagePlayer (10);
+		}
+	}
+}
