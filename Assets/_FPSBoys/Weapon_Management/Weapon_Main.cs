b6a952f0b70a43e57fe3dafc51841a7184021dfb
Retus001 <retus001@gmail.com>
2018-05-16 20:47:55 -0500

    Various
    Misc Testing
    Organized Weapon Management
    Organized Player Prefab

---
+++ Assets/_FPSBoys/Weapon_Management/Weapon_Main.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Weapon_Main : MonoBehaviour {
+
+	// Class Weapon
+
+	[Header("Variables de todas las armas")]
+	public Transform spawnPoint;
+	public float firerate = 0.1f;
+	internal float lastShootTime;
+
+	public virtual void OnShootDown()
+	{
+
+	}
+
+	public virtual void OnShoot()
+	{
+
+	}
+
+	public virtual void OnShootUp()
+	{
+
+	}
+
+	public virtual void Shoot()
+	{
+		if (Time.time > lastShootTime + firerate)
+		{
+			ExecuteShoot ();
+		}
+	}
+
+	public virtual void Reload()
+	{
+
+	}
+
+	public virtual void Sheathe ()
+	{
+
+	}
+
+	public virtual void UnSheathe ()
+	{
+
+	}
+
+	public virtual void ExecuteShoot()
+	{
+		lastShootTime = Time.time;
+	}
+}
