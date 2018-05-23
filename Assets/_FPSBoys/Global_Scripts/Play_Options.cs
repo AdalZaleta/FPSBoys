fbe31ad268c733a8a948092f237d181c5f62668b
AdalZaleta <32682010+AdalZaleta@users.noreply.github.com>
2018-05-23 16:07:53 -0500

    Agregando las cosas del inicio
    Se agrego musica :musical_note:

---
+++ Assets/_FPSBoys/Global_Scripts/Play_Options.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace FPSBoys
+{
+	public class Play_Options : MonoBehaviour {
+
+		public int actualCharacter;
+
+		public void setCurrentCharacter(int current)
+		{
+			actualCharacter = current;
+		}
+	}
+}
--- Assets/_FPSBoys/Managers/Manager_Scene.cs
+++ Assets/_FPSBoys/Managers/Manager_Scene.cs
@@ -1,16 +1,33 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 namespace FPSBoys
 {
 	public class Manager_Scene : MonoBehaviour {
 
+		public GameObject options;
+
 		void Awake()
 		{
 			Manager_Static.scenManager = this;
 		}
+
+		public void LoadScene(int _scene)
+		{
+			SceneManager.LoadScene (_scene, LoadSceneMode.Single);
+		}
+
+		public void QuitGame()
+		{
+			Application.Quit ();
+		}
+
+		public void LoadGame(int _character)
+		{
+			options.GetComponent<Play_Options> ().setCurrentCharacter (_character);
+			DontDestroyOnLoad (options.GetComponent<Play_Options> ());
+		}
 	}
-
-
 }
